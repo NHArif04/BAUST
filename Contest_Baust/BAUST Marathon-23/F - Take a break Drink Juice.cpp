cb299c67e676e2146567ac12a5739ba18acdf576
nharif04 <nahidhasanarif03@gmail.com>
2023-09-14 16:35:31 +0600

    3rd place in contest

--- Contest_Baust/BAUST Marathon-23/F - Take a break Drink Juice.cpp
+++ Contest_Baust/BAUST Marathon-23/F - Take a break Drink Juice.cpp
@@ -8,9 +8,9 @@ int main(){
     cin>>test;
     for(int i=1; i<=test; i++){
         cin >>R1>>R2>>H>>P;
-        double Rp = R1 + (R2-R1)*P/H;
-        double volume = (M_PI*P/3)*(pow(R2,2)+pow(Rp,2)+R2*Rp);
-        cout<<"Case "<<i<<": "<<fixed<<setprecision(8)<<volume<<endl;
+        double Rp = R2 + ((R1-R2)*P)/(H*1.0);
+        double volume = (M_PI*P/3.0)*(pow(R2,2)+pow(Rp,2)+(R2*Rp));
+        cout<<"Case "<<i<<": "<<fixed<<setprecision(9)<<volume<<endl;
     }
     return 0;
 }
